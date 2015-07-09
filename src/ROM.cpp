1969f74e6b071221d00707a7b168f5ec53db55fa
ibon <ibicano@yahoo.es>
2015-07-09 10:46:47 +0200

    Implementada la clase ROM.

--- src/ROM.cpp
+++ src/ROM.cpp
@@ -6,32 +6,212 @@
  */
 
 #include "ROM.hpp"
+#include <fstream>
 
 
-ROM::ROM() {
-	// TODO Auto-generated constructor stub
+ROM::ROM(string fileName) {
+	rom = new vector();
+
+	prgCount = 0;
+	chrCount = 0;
+	control1 = 0x00;
+	control2 = 0x00;
+	ramCount = 0;
+
+	prgBanks = new int*[prgCount];
+	chrBanks = new int*[chrCount];
+	chrBanks = new int*[ramCount];
+
+	loadOk = false;
+
+	loadFile(fileName);
 }
 
 ROM::~ROM() {
-	// TODO Auto-generated destructor stub
+	for (int i = 0; i < prgCount; i++) {
+		if (prgBanks[i] != NULL) delete []prgBanks[i];
+	}
+
+	for (int i = 0; i < chrCount; i++) {
+		if (chrBanks[i] != NULL) delete []chrBanks[i];
+	}
 }
 
-int ROM::loadFile(string filename) {
-	return 0;
-}
+int ROM::loadFile(string fileName) {
 
+	// Lee el fichero de la ROM y lo almacena en un vector en memoria para poder parsearlo
+	ifstream f(fileName);
+	char byte;
+	while (f.get(byte)) {
+		rom->push_back(byte);
+	}
+	f.close();
+
+	string s = "";
+	for (int i= 0; i < 3; i++) {
+		s = s + rom[i];
+	}
+
+	if (s == "NES" && (unsigned int)rom[3] == 0x1A) {
+		// Carga la cabecera
+		prgCount = rom[4];
+		chrCount = rom[5];
+		control1 = rom[6];
+		control2 = rom[7];
+		ramCount = rom[8];
+		//reserved = rom[9:16]
+
+		// Reserva la memoria para almacenar los bancos en función del número de estos
+		for (int i = 0; i < prgCount; i++) {
+			if (prgBanks[i] != NULL) delete []prgBanks[i];
+			prgBanks[i] = new int[ROM::PRG_SIZE];
+		}
+
+		for (int i = 0; i < chrCount; i++) {
+			if (chrBanks[i] != NULL) delete []chrBanks[i];
+			chrBanks[i] = new int[ROM::CHR_SIZE];
+		}
+
+		// Carga el modo de mirroring
+		if (getControl1MirroringBit3() == 0)
+			mirroring = getControl1MirroringBit0();
+		else
+			mirroring = 0x03;
+
+		// Cargamos los datos de la ROM. La variable "i" es el índice de lectura
+		int i = 16;		// Empezamos después de las cabeceras
+		int end = i + 512;
+
+		// Si hay un trainer se carga primero
+		if (getControl1TrainerBit2()) {
+			while (i < end) {
+				trainer[i] = rom[i];
+				i++;
+			}//while
+		}//if
+
+		// Carga los bancos PRG
+		for (int n = 0; n < prgCount; n++) {
+			prgBanks[n] = new int[16384];
+
+			int j = 0;
+			while (j < 16384) {
+				prgBanks[n][j] = rom[i];
+				i++;
+				j++;
+			}//while
+		}//for
+
+		// Carga los bancos CHR
+		for (int n = 0; n < chrCount; n++) {
+			chrBanks[n] = new int[8192];
+
+			int j = 0;
+			while (j < 8192) {
+				prgBanks[n][j] = rom[i];
+				i++;
+				j++;
+			}//while
+		}//for
+
+		loadOk = true;
+	}//if
+	else {
+		loadOk = false;
+		cout << "Formato de fichero incorrecto";
+	}//else
+}//loadFile()
+
+
+/* Devuelve el modo mirroring especificado en la ROM:
+   0x00: horizontal
+   0x01: vertical
+   0x02: single
+   0x03: 4-screen
+*/
 int ROM::getMirroring() {
-	return 0;
+	return mirroring;
 }
+
 
 int ROM::getMapperCode() {
-	return 0;
+	int mapperCode = control1 >> 4;
+	int mapperCode = mapperCode | (control2 & 0xF0);
+
+	return mapperCode;
 }
+
 
 int ROM::getPrgCount() {
-	return 0;
+	return prgCount;
 }
 
+
 int ROM::getChrCount() {
-	return 0;
+	return chrCount;
+}
+
+
+int* ROM::getPrg(int n) {
+	return prgBanks[n];
+}
+
+
+int* ROM::getPrg8k(int n) {
+	int number = n >> 1;
+	int part = n & 0x01;
+
+	int* bank;
+
+	if (part == 0)
+		bank = prgBanks[number][0];
+	else
+		bank = prgBanks[number][8192];
+
+	return bank;
+}
+
+
+int* ROM::getChr(int n) {
+	return chrBanks[n];
+}
+
+
+int* ROM::getChr4k(int n) {
+	int number = n / 2;
+	int* bank;
+
+	// Si es par es la primera mitad del banco de 8k
+	if (n & 0x01 == 0)
+		bank = chrBanks[number][0x0000];
+	else
+		bank = chrBanks[number][0x1000];
+
+	return bank;
+}
+
+
+int* ROM::getChr1k(int n) {
+	int number = n >> 3;
+	int part = n & 0x07;
+	int addr = part * 1024;
+
+	int* bank = chrBanks[number][addr];
+
+	return bank;
+}
+
+
+int ROM::getControl1MirroringBit0() {
+	return control1 & 0x01;
+}
+
+
+int ROM::getControl1MirroringBit3() {
+	return control1 & 0x08 >> 3;
+}
+
+
+int ROM::getControl1TrainerBit2() {
+	return control1 & 0x04 >> 2;
 }
--- src/ROM.hpp
+++ src/ROM.hpp
@@ -9,16 +9,20 @@
 #define ROM_HPP_
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class ROM {
 public:
-	ROM();
+	static const int PRG_SIZE = 16384;
+	static const int CHR_SIZE = 8192;
+
+	ROM(string fileName);
 	virtual ~ROM();
 
 	// Carga la ROM de un fichero
-	int loadFile(string filename);
+	int loadFile(string fileName);
 
 	/*
 	 * Devuelve el modo de mirroring indicado en la ROM:
@@ -38,7 +42,53 @@ public:
 	// Devuelve el número de bancos de patrones de 8KB presentes en la ROM
 	int getChrCount();
 
+	// Devuelve el banco PRG número n
+	int* getPrg(int n);
+
+	// Devuelve un banco PRG con direccionamiento como si fuesen de 8k en vez de 16k
+    int* getPrg8k(int n);
+
+	// Devuelve el banco CHR número n
+	int* getChr(int n);
+
+	// Devuelve un banco CHR con direccionamiento como si fuesen de 4k en vez de 8k
+    int* getChr4k(int n);
+
+    // Devuelve un banco CHR con direccionamiento como si fuesen de 1k en vez de 8k
+    int* getChr1k(int n);
+
 	//TODO: Acabar la especificación de las funciones
+
+private:
+	// Contenido de la ROM
+	vector<char>* rom;
+
+	int prgCount;		// Número de bancos de 16KB de ROM de programa
+	int chrCount;		// Número de bancos de 8KB de ROM de patrones
+	int control1;		// Byte de control 1
+	int control2;		// Byte de control 2
+	int ramCount;		// Bytes de memoria RAM
+
+	// Array que almacena el trainer, en caso de que lo haya
+	int trainer[512];
+
+	// Arrays que almacenan los bancos de rom
+	int** prgBanks;
+	int** chrBanks;
+	int** ramBanks;
+
+	//Indica si la ROM se ha cargado correctamente
+	bool loadOk;
+
+	// Almacena el modo de mirroring indicado en la ROM
+	int mirroring;
+
+	// Funciones que devuelven los bits de mirroring del registro de control 1
+	int getControl1MirroringBit0();
+	int getControl1MirroringBit3();
+
+	// Devuelve el bit que indica si hay un trainer en el registro de control 1
+	int getControl1TrainerBit2();
 
 };
 
