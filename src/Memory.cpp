44b81482c2881aee0c222bb18a06f17a33a6beaf
ibon <ibicano@yahoo.es>
2015-07-09 09:39:09 +0200

    Implementada la memoria principal.

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -7,12 +7,74 @@
 
 #include "Memory.hpp"
 
-Memory::Memory() {
-	// TODO Auto-generated constructor stub
+Memory::Memory(PPU* ppu, Mapper* mapper, Joypad* joypad1) {
+	// Inicializa el aray de memoria
+	for (int i = 0; i < Memory::SIZE; i++)
+		memory[i] = 0x00;
 
-}
+	this->ppu = ppu;
+	this->mapper = mapper;
+	this->joypad1 = joypad1;
+}//Memory()
+
 
 Memory::~Memory() {
-	// TODO Auto-generated destructor stub
-}
+}//~Memory()
 
+
+/*
+ * Lee la posición de memoria indicada por el parámetro 'addr', que
+ * tiene que se un número de 16 bits, y devuelve el contenido
+ */
+int Memory::readData(int addr) {
+	int d = 0x00;
+	if (addr >= 0x8000)      // Memoria del programa-> Lee del mapper de la ROM
+		d = mapper->readPrg(addr);
+	else if (addr >= 0x0000 && addr < 0x2000)      // Memoria RAM
+		d = memory[addr];
+	else if (addr >= 0x2000 && addr < 0x4000)      // Direcciones de los registros PPU
+		d = ppu->readReg(0x2000 + (addr & 0x07));
+	else if (addr >= 0x4000 && addr < 0x4020) {		// Más registros I/O
+		if (addr == 0x4016)      					// Registro del Joypad 1
+			d = joypad1->readReg();
+		else if (addr == 0x4017) {}
+	}
+	else if (0x6000 <= addr <= 0x7FFF)     			// Memoria de estado de la partida->
+		d = memory[addr];
+}//readData()
+
+
+/*
+ * Escribe el dato 'data' en la posición de memoria 'addr'
+ */
+void Memory::writeData(int data, int addr) {
+	int d = data & 0xFF;
+	addr = addr & 0xFFFF;
+
+	int n;
+	if (addr >= 0x0000 && addr < 0x2000) {        // Espacio de memoria RAM
+		n = addr & 0x7FF;
+		memory[n] = d;
+		memory[0x0800 + n] = d;
+		memory[0x1000 + n] = d;
+		memory[0x1800 + n] = d;
+	}
+	else if (addr >= 0x2000 && addr < 0x4000) {     // Direcciones de los registros PPU
+		n = 0x2000 + (addr & 0x07);
+		ppu->writeReg(d, n);
+	}
+	else if (addr >= 0x4000 and addr <= 0x401F) {    // Más registros I/O
+		if (addr == 0x4014)      // Escritura de memoria de Sprites por DMA
+			ppu->writeSpriteDma(this, d);
+		else if (addr == 0x4016)        // Joypad 1
+			joypad1->writeReg(d);
+		else if (addr == 0x4017) {}        // Joypad 2
+	}
+	else if (0x6000 <= addr <= 0x7FFF)
+		memory[addr] = d;
+	else if (addr >= 0x8000) {           // Memoria de programa-> Escribe al mapper->
+		mapper->writePrg(d, addr);
+		// reseteamos la cache de Tiles por si hay un cambio de banco en el Mapper
+		ppu->resetTilesCache();
+	}
+}//writeData()
--- src/Memory.hpp
+++ src/Memory.hpp
@@ -10,7 +10,11 @@
 
 class Memory {
 public:
-	Memory();
+	static const int SIZE = 0x10000;
+
+	// Se le pasan referencias de los objetos que se mapean en memoria
+	Memory(PPU* ppu, Mapper* mapper, Joypad* joypad1);
+
 	virtual ~Memory();
 
 	// Lee una posición de memoria
@@ -18,6 +22,13 @@ public:
 
 	// Escribe un dato en una posición de memoria
 	void writeData(int data, int addr);
+
+private:
+	int memory[Memory::SIZE];	// Almacena los datos
+
+	PPU* ppu;
+	Mapper* mapper;
+	Joypad* joypad1;
 };
 
 #endif /* MEMORY_HPP_ */
