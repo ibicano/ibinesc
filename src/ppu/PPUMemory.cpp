167b147e0a6c83c101cfd4dfb393802e421ebbbf
Ibon <ibicano@gmail.com>
2015-07-10 17:39:55 +0200

    Completada implementación de PPUMemory.

--- src/ppu/PPUMemory.cpp
+++ src/ppu/PPUMemory.cpp
@@ -48,134 +48,152 @@ void PPUMemory::writeData(int data, int addr) {
 	else if (0x2000 <= a < 0x4000) {
 		// Name Table 0
 		if (0x2000 <= a < 0x2400) {
-			if (mapper->mirrormode() == 0) {
-				setmemory(d, a + 0x0400)
+			if (mapper->mirrorMode() == 0) {
+				setMemory(d, a + 0x0400);
 				// Mirrors
-				setmemory(d, a + 0x1000)
-				setmemory(d, a + 0x1400)
-			}
-			elif (mapper->mirrormode() == 1) {
-				setmemory(d, a + 0x0800)
+				setMemory(d, a + 0x1000);
+				setMemory(d, a + 0x1400);
+			}//if
+			else if (mapper->mirrorMode() == 1) {
+				setMemory(d, a + 0x0800);
 				// Mirrors
-				setmemory(d, a + 0x1000)
-				setmemory(d, a + 0x1800)
-			elif mapper->mirrormode() == 2:
-				setmemory(d, a + 0x0400)
-				setmemory(d, a + 0x0800)
+				setMemory(d, a + 0x1000);
+				setMemory(d, a + 0x1800);
+			}//else if
+			else if (mapper->mirrorMode() == 2) {
+				setMemory(d, a + 0x0400);
+				setMemory(d, a + 0x0800);
 				// Mirrors
-				setmemory(d, a + 0x1000)
-				setmemory(d, a + 0x1400)
-				setmemory(d, a + 0x1800)
-
+				setMemory(d, a + 0x1000);
+				setMemory(d, a + 0x1400);
+				setMemory(d, a + 0x1800);
+			}//else if
 			// Escribimos en la posición indicada
-			setmemory(d, a)
-		}
+			setMemory(d, a);
+		}//if
 		// Name Table 1
-		elif 0x2400 <= a < 0x2800:
-			if mapper->mirrormode() == 0:
-				setmemory(d, a - 0x0400)
+		else if (0x2400 <= a < 0x2800) {
+			if (mapper->mirrorMode() == 0) {
+				setMemory(d, a - 0x0400);
 				// Mirrors
-				setmemory(d, a + 0x1000)
-				setmemory(d, a + 0x0C00)
-			elif mapper->mirrormode() == 1:
-				setmemory(d, a + 0x0800)
+				setMemory(d, a + 0x1000);
+				setMemory(d, a + 0x0C00);
+			}
+			else if (mapper->mirrorMode() == 1) {
+				setMemory(d, a + 0x0800);
 				// Mirrors
-				setmemory(d, a + 0x1000)
-				if a < 0x2700:
-					setmemory(d, a + 0x1800)
-			elif mapper->mirrormode() == 2:
-				setmemory(d, a - 0x0400)
-				setmemory(d, a + 0x0800)
+				setMemory(d, a + 0x1000);
+				if (a < 0x2700)
+					setMemory(d, a + 0x1800);
+			}
+			else if (mapper->mirrorMode() == 2) {
+				setMemory(d, a - 0x0400);
+				setMemory(d, a + 0x0800);
 				// Mirrors
-				setmemory(d, a + 0x1000)
-				setmemory(d, a + 0x0C00)
-				if a < 0x2700:
-					setmemory(d, a + 0x1800)
-
+				setMemory(d, a + 0x1000);
+				setMemory(d, a + 0x0C00);
+				if (a < 0x2700)
+					setMemory(d, a + 0x1800);
+			}
 			// Escribimos en la posición indicada
-			setmemory(d, a)
+			setMemory(d, a);
+		}//else if
 		// Name Table 2
-		elif 0x2800 <= a < 0x2C00:
-			if mapper->mirrormode() == 0:
-				setmemory(d, a + 0x0400)
+		else if (0x2800 <= a < 0x2C00) {
+			if (mapper->mirrorMode() == 0) {
+				setMemory(d, a + 0x0400);
 				// Mirrors
-				setmemory(d, a + 0x1000)
-				if a < 0x2B00:
-					setmemory(d, a + 0x1400)
-			elif mapper->mirrormode() == 1:
-				setmemory(d, a - 0x0800)
+				setMemory(d, a + 0x1000);
+				if (a < 0x2B00)
+					setMemory(d, a + 0x1400);
+			}
+			else if (mapper->mirrorMode() == 1) {
+				setMemory(d, a - 0x0800);
 				// Mirrors
-				setmemory(d, a + 0x1000)
-				setmemory(d, a + 0x0800)
-			elif mapper->mirrormode() == 2:
-				setmemory(d, a + 0x0400)
-				setmemory(d, a - 0x0800)
+				setMemory(d, a + 0x1000);
+				setMemory(d, a + 0x0800);
+			}
+			else if (mapper->mirrorMode() == 2) {
+				setMemory(d, a + 0x0400);
+				setMemory(d, a - 0x0800);
 				// Mirrors
-				setmemory(d, a + 0x1000)
-				if a < 0x2B00:
-					setmemory(d, a + 0x1400)
-				setmemory(d, a + 0x0800)
-
+				setMemory(d, a + 0x1000);
+				if (a < 0x2B00)
+					setMemory(d, a + 0x1400);
+				setMemory(d, a + 0x0800);
+			}
 			// Escribimos en la posición indicada
-			setmemory(d, a)
+			setMemory(d, a);
+		}//else if
 		// Name Table 3
-		elif 0x2C00 <= a < 0x3000:
-			if mapper->mirrormode() == 0:
-				setmemory(d, a - 0x0400)
+		else if (0x2C00 <= a < 0x3000) {
+			if (mapper->mirrorMode() == 0) {
+				setMemory(d, a - 0x0400);
 				// Mirrors
-				if a < 0x2F00:
-					setmemory(d, a + 0x1000)
-				setmemory(d, a + 0x0C00)
-			elif mapper->mirrormode() == 1:
-				setmemory(d, a - 0x0800)
+				if (a < 0x2F00)
+					setMemory(d, a + 0x1000);
+				setMemory(d, a + 0x0C00);
+			}
+			else if (mapper->mirrorMode() == 1) {
+				setMemory(d, a - 0x0800);
 				// Mirrors
-				if a < 0x2F00:
-					setmemory(d, a + 0x1000)
-				setmemory(d, a + 0x0800)
-			elif mapper->mirrormode() == 2:
-				setmemory(d, a - 0x0400)
-				setmemory(d, a - 0x0800)
+				if (a < 0x2F00)
+					setMemory(d, a + 0x1000);
+				setMemory(d, a + 0x0800);
+			}
+			else if (mapper->mirrorMode() == 2) {
+				setMemory(d, a - 0x0400);
+				setMemory(d, a - 0x0800);
 				// Mirrors
-				if a < 0x2F00:
-					setmemory(d, a + 0x1000)
-				setmemory(d, a + 0x0C00)
-				setmemory(d, a + 0x0800)
-
+				if (a < 0x2F00)
+					setMemory(d, a + 0x1000);
+				setMemory(d, a + 0x0C00);
+				setMemory(d, a + 0x0800);
+			}
 			// Escribimos en la posición indicada
-			setmemory(d, a)
-	}
+			setMemory(d, a);
+		}
 		// Mirrors Name Tables y Attr Tables
-		elif 0x3000 <= a < 0x3F00:
-			writedata(d, a - 0x1000)
+		else if (0x3000 <= a < 0x3F00) {
+			writeData(d, a - 0x1000);
+		}
 		// Paletas
-		elif 0x3F00 <= a < 0x3F20:
+		else if (0x3F00 <= a < 0x3F20) {
 			// Si se escribe en el elemento de background o su mirror se escribe el valor de background
 			// en todas las paletas mod 4 (pero no al contrario)
-			if a == 0x3F00 or a == 0x3F10:
-				for x in xrange(0x3F00, 0x4000, 0x04):
-					setmemory(d, x)
+			if (a == 0x3F00 or a == 0x3F10) {
+				for (int x = 0x3F00; x < 0x4000; x = x + 0x04)
+					setMemory(d, x);
+			}
 			// Si no es un elemento de background escribimos normalmente la paleta
-			elif a & 0x03 != 0:
+			else if (a & 0x03 != 0) {
 				// Escribe en mirrors
-				setmemory(d, a + 0x0020)
-				setmemory(d, a + 0x0040)
-				setmemory(d, a + 0x0060)
-				setmemory(d, a + 0x0080)
-				setmemory(d, a + 0x00A0)
-				setmemory(d, a + 0x00C0)
-				setmemory(d, a + 0x00E0)
+				setMemory(d, a + 0x0020);
+				setMemory(d, a + 0x0040);
+				setMemory(d, a + 0x0060);
+				setMemory(d, a + 0x0080);
+				setMemory(d, a + 0x00A0);
+				setMemory(d, a + 0x00C0);
+				setMemory(d, a + 0x00E0);
 
 				// Escribimos en la posición indicada
-				setmemory(d, a)
+				setMemory(d, a);
+			}//else if
+		}
 		// Mirrors paletas
-		elif 0x3F20 <= a < 0x4000:
-			writedata(d, ((a - 0x3F20) % 0x0020) + 0x3F00)
-
+		else if (0x3F20 <= a < 0x4000)
+			writeData(d, ((a - 0x3F20) % 0x0020) + 0x3F00);
+	}
 	// Mirrors generales
-	elif a >= 0x4000:
-		writedata(d, a & 0x3FFF)
+	else if (a >= 0x4000)
+		writeData(d, a & 0x3FFF);
 }//writeData()
 
 
 void PPUMemory::setMemory(int data, int addr) {
-}
+    int a = addr & 0x3FFF;
+    memory[a] = data;
+    memory[a + 0x4000] = data;
+    memory[a + 0x8000] = data;
+    memory[a + 0xC000] = data;
+}//setMemory()
